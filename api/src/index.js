85315d0983fef1c4b7d67467c3def6fce6d06fa5
Osama Salama <osama.m7@gmail.com>
2018-06-04 12:48:46 +0200

    adding cross env babel and more dev dependencies

--- api/src/index.js
+++ api/src/index.js
@@ -1,19 +1,21 @@
 'use strict';
+
+const { Router } = require('express') ;
+const router = new Router();
 const version = 1.0;
-const Router = require('express');
+
 const config = require('../../config/config');
 
-module.exports = () => {
-    
-    let api = Router();
-    // mount the facets resource
-    api.use('/', 'hello from API');
-    
-    // perhaps expose some API metadata at the root
-    api.get('/', (req, res) => {
-        res.json({ version });
-});
-    
-    return api;
+
+function getPosts (req, res) {
+res.status(200).send('hello');
 };
 
+
+
+// Get all Posts
+router.route('/').get(getPosts);
+
+// Get one post by cuid
+checkSpacingForModuleDeclaration
+export router;
--- index.js
+++ index.js
@@ -15,9 +15,7 @@ app.server = http.createServer(app);
 app.use(morgan('dev'));
 
 // 3rd party middleware
-app.use(cors({
-    exposedHeaders: config.corsHeaders
-}));
+
 
 app.use(bodyParser.json({
     limit : config.bodyLimit
@@ -42,7 +40,7 @@ initialize( () => {
     app.use('/', api({ config }));
     
     app.server.listen(process.env.PORT || config.port, () => {
-        console.log(`Started on port ${app.server.address().port}`);
+        console.log(`Started on port ${app.port}`);
     });
 });
 
