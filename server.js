0e055f48de6405302e21108c6e36847f8ed2f9cb
Osama Salama <osama.m7@gmail.com>
2018-06-18 14:34:21 +0200

    using a global mongodb connection and session

--- server.js
+++ server.js
@@ -3,14 +3,39 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const api = require('./api/src/index');
 const compression =require('compression');
+
 const colors = require('colors');
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+const api = require('./api/src/index');
+const MongoAdapter = require('./api/src/mongodb-adapter');
 
 const Config = require('./config/config.json');
 const app = new express();
 
 
+let mongoConnection = {};
+
+let MongoClient = function () {
+	return MongoAdapter.connect()
+			.then( connection => {
+			return connection.db(Config.mongodb.dbName);
+			}).catch( (err) =>{
+			console.log(err);
+		});
+};
+
+
+
+app.use(session({
+	secret: 'foo',
+	store: new MongoStore({ dbPromise: MongoClient() }),
+	resave: false, //don't save session if unmodified
+	saveUninitialized: false,
+}));
 app.use(compression());
 app.use(bodyParser.json({ limit: '20mb' }));
 app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
