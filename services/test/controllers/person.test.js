2458abb3cfee4017ab8644c47dc8baba7aa7628c
Osama Salama <osama.m7@gmail.com>
2018-06-19 15:11:44 +0200

    Adding person test

---
+++ services/test/controllers/person.test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const expect = chai.expect;
+const chaiHttp = require('chai-http');
+const Config = require('../../../config/config.json');
+const PersonController = require('../../src/controllers/person/person');
+
+const should = chai.should();
+
+
+describe('Person controller', () => {
+
+	beforeEach( (done) => {
+
+		done();
+	});
+
+	describe('Add a new person', () => {
+		it('It should connect to db and add a new person', async (done) => {
+			let result = await PersonController.addPerson({'foo':'bar'});
+			expect(result).to.be.ok;
+			expect(result).to.be.an('object');
+
+			done();
+		});
+	});
+
+});
