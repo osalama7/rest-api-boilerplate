5e5938f90906bfb8171639be29eb9a4ad5bf2be5
Osama Salama <osama.m7@gmail.com>
2018-07-14 12:51:04 +0200

    updating mongodb driver

--- api/src/mongodb-adapter.js
+++ api/src/mongodb-adapter.js
@@ -1,32 +1,38 @@
 'use strict';
 const MongoClient = require('mongodb').MongoClient;
-const Config = require('../../config/config.json');
+
 const assert = require('assert');
 const colors = require('colors');
 
-
-
+const Config = process.env.NODE_ENV === 'test'
+		? require('./../../config/config.json').env.test
+		: require('./../../config/config.json').env.dev;
+const connection = {};
 // Initialize Mongodb
 module.exports.connect = async () => {
-	let db = {};
-	db = await MongoClient
-			.connect(Config.env.dev.mongodb.url, {})
-			.then( connection => {
-				return connection;
+	await MongoClient
+			.connect(Config.mongodb.url, {})
+			.then( async db => {
+				connection.db = await db.db(Config.mongodb.dbName)
+
 			})
 			.catch(function (err) {
 				console.error(colors.red('Could not connect to MongoDB!'));
 				console.log(err);
 			});
-	return db;
+
+
 };
 
-module.exports.disconnect = (cb) => {
-	return MongoClient.connection.db
+module.exports.disconnect = (connection,  cb) => {
+	return connection.db
 			.close( (err) => {
-
 				console.info(colors.yellow('Disconnected from MongoDB'));
-
 				return cb(err);
 			});
 };
+
+
+
+
+module.exports.connection  = connection;
