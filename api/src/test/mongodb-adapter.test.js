89f45b360b43c76d2414d50309dea75e874d4456
Osama Salama <osama.m7@gmail.com>
2018-07-14 12:51:19 +0200

    updating mongodb test

--- api/src/test/mongodb-adapter.test.js
+++ api/src/test/mongodb-adapter.test.js
@@ -6,25 +6,24 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const MongoAdapter = require('../mongodb-adapter');
 const should = chai.should();
-
+const Config = require('../../../config/config.json').env.test;
 
 describe('Mongodb Adapter', () => {
 
 	beforeEach((done) => {
-
 		done();
 	});
 
 	describe('Connect to mongodb', () => {
-		it('It should connect to db', async (	done) => {
-			let connection = await MongoAdapter
+		it('It should connect to db', async (done) => {
+			await MongoAdapter
 					.connect()
 					.catch((err) => {
 						console.log(err);
 			});
-			const db = await connection
-					.db('test');
-			db.should.be.ok;
+
+			MongoAdapter.connection.db.should.be.ok;
+			MongoAdapter.connection.db.should.be.a('object');
 			done();
 		});
 	});
